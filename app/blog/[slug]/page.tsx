f6462babdfd286ec33ca91434c5e45b2747f4e0f
HarshMehta9000 <145385402+HarshMehta9000@users.noreply.github.com>
2025-05-17 22:06:45 -0500

    Update page.tsx

--- app/blog/[slug]/page.tsx
+++ app/blog/[slug]/page.tsx
@@ -55,7 +55,6 @@ export async function generateMetadata({ params }: BlogPostPageProps): Promise<M
 export default async function BlogPostPage({ params }: BlogPostPageProps) {
   const posts = await getAllBlogPosts()
   const post = posts.find((post) => post.slug === params.slug)
-
   if (!post) {
     notFound()
   }

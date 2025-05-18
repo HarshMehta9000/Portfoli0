ba0fca1c9f105ec51864dfeb4793c76d83fb5722
HarshMehta9000 <145385402+HarshMehta9000@users.noreply.github.com>
2025-05-18 00:41:46 -0500

    Update projects.ts

--- lib/projects.ts
+++ lib/projects.ts
@@ -2,7 +2,7 @@
 
 export async function getAllProjects() {
   const res = await fetch(
-    "https://o0spbb2buxzewjsk.public.blob.vercel-storage.com/projects.json", 
+    "https://o0spbb2buxzewjsk.public.blob.vercel-storage.com/projects-TfdZQWYgTXnkl4DR2epsjO6kRcqF9q.json", 
     { cache: "no-store" }
   );
   if (!res.ok) throw new Error("Failed to fetch projects");
